69fad8ebebe9ad109906ffd485ab807e881a997c
sergey-laschotko <anaharet@tut.by>
2018-12-13 10:54:45 +0300

    Added modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,10 @@ import $ from "jquery";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "75%");
 const stickyHeader = new StickyHeader();
+const modal = new Modal();
